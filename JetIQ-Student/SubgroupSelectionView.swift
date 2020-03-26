56d4d45107ed7be7a477cb4a5bf773d9b84947a0
max <mxx888777@gmail.com>
2020-03-26 07:28:05 -0700

    Use identifiable array in sebgroup selection

--- JetIQ-Student/SubgroupSelectionView.swift
+++ JetIQ-Student/SubgroupSelectionView.swift
@@ -12,19 +12,19 @@ struct SubgroupSelectionView: View
 {
     @EnvironmentObject var userData:UserData
     
-    @State private var selectedStrength = ""
+    @State private var selectedSubgroup = ""
+    
+    let subgroups = ["None", "1", "2", "3", "4", "5"]
     
     var body: some View
     {
      VStack
         {
             Text("Select Subgroup:")
-            Picker(selection: $selectedStrength, label: Text(""))
+            Picker(selection: $selectedSubgroup, label: Text(""))
             {
-                Text("None")
-                ForEach(1 ..< 6)
-                {
-                    Text(String($0))
+                ForEach(subgroups, id: \.self) { (string: String) in
+                    Text(string)
                 }
             }
             Button(action: {self.setSubgroup()})
@@ -43,7 +43,9 @@ struct SubgroupSelectionView: View
     func setSubgroup()
     {
         DispatchQueue.main.async { [self] in
-            self.userData.subgroup = self.$selectedStrength.wrappedValue
+            
+            //self.userData.subgroup = self.selectedStrength
+            self.userData.subgroup = self.selectedSubgroup
             saveUserData(userData: self.userData)
         }
         
