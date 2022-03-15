626dec4f2eb2278575b3b702d655aa4e058343e8
ZeeWanderer <mxx888777@gmail.com>
2022-03-15 05:27:51 -0700

    [Build] remove build increment, it is managed automatically Serverside

--- Scripts/increment_build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh -ex
-if [ ! "$IS_AZURE" = 'true' -a ! "$ACTION" = 'install'  ] ;
-then
-   currentNumber=$(/usr/libexec/PlistBuddy -c "Print :CFBundleVersion" "$PRODUCT_SETTINGS_PATH")
-   buildNumber=$(git rev-list --count HEAD)
-   if [ "$buildNumber" -ne "$currentNumber" ] ; then
-      /usr/libexec/PlistBuddy -c "Set :CFBundleVersion $buildNumber" "$PRODUCT_SETTINGS_PATH"
-   fi
-fi
